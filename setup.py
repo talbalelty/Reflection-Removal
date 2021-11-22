bb4751cdb5029901e406bde304c149d95b32a820
Tal Balelty <mrtal1994@gmail.com>
2021-11-22 09:51:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='video_separation',
     version='0.2.3',
-    url='https://github.com/talbalelty/Reflection-Removal',
+    url='https://github.com/talbalelty/video_separation',
     license='MIT',
     packages=setuptools.find_packages(),
     install_requires=['opencv-python', 'numpy'],
