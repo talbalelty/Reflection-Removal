72c1b2d4ebe3fdc266b194e69ec508317a4b0103
ben.koren <ben.koren@urban-dashboard.com>
2022-06-08 12:42:20 +0300

    removing spaces

--- video_separation/video_separation.py
+++ video_separation/video_separation.py
@@ -20,13 +20,16 @@ class VideoSeparation:
         self.get_reflection()
         self.reflection_video = open(self.path_to_reflection_video, 'r')
 
+
     def get_transmission(self):
-        self.minimum_layer = self.process_video_separation(cv2.VideoCapture(self.path_to_stabilized_video), self.extract_minimum_layer,
+        self.minimum_layer = self.process_video_separation(cv2.VideoCapture(self.path_to_stabilized_video),
+                                                           self.extract_minimum_layer,
                                                            self.path_to_transmission_video)
         return cv2.VideoCapture(self.path_to_transmission_video)
 
     def get_reflection(self):
-        self.maximum_layer = self.extract_maximum_layer(cv2.VideoCapture(self.path_to_stabilized_video), self.minimum_layer,self.path_to_reflection_video)
+        self.maximum_layer = self.extract_maximum_layer(cv2.VideoCapture(self.path_to_stabilized_video),
+                                                        self.minimum_layer, self.path_to_reflection_video)
         return cv2.VideoCapture(self.path_to_reflection_video)
 
     def save_video(self, video, fps, path):
@@ -120,106 +123,76 @@ class VideoSeparation:
 
     def stabilizeVideo(self):
         # smooth_amount - The larger the more stable the video, but less reactive to sudden panning
-
         # Read input video
         cap = cv2.VideoCapture(self.path_to_video)
-
         # Get frame count
         n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
         # Get width and height of video stream
         w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
         # Get frames per second (fps)
         fps = cap.get(cv2.CAP_PROP_FPS)
-
         # Define the codec for output video
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-
         # Set up output video
         out = cv2.VideoWriter(self.path_to_stabilized_video, fourcc, fps, (h, w))
-
         # Read first frame
         _, prev = cap.read()
-
         # Convert frame to grayscale
         prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
-
         # Pre-define transformation-store array
         transforms = np.zeros((n_frames - 1, 3), np.float32)
-
+        #
         for i in range(n_frames - 2):
             # Detect feature points in previous frame
-            prev_pts = cv2.goodFeaturesToTrack(prev_gray,
-                                               maxCorners=200,
-                                               qualityLevel=0.01,
-                                               minDistance=30,
-                                               blockSize=3)
-
+            prev_pts = cv2.goodFeaturesToTrack(prev_gray,maxCorners=200,qualityLevel=0.01,minDistance=30,blockSize=3)
             # Read next frame
             success, curr = cap.read()
             if not success:
                 break
-
-                # Convert to grayscale
+            # Convert to grayscale
             curr_gray = cv2.cvtColor(curr, cv2.COLOR_BGR2GRAY)
-
             # Calculate optical flow (i.e. track feature points)
             curr_pts, status, err = cv2.calcOpticalFlowPyrLK(prev_gray, curr_gray, prev_pts, None)
-
             # Sanity check
             assert prev_pts.shape == curr_pts.shape
-
             # Filter only valid points
             idx = np.where(status == 1)[0]
             prev_pts = prev_pts[idx]
             curr_pts = curr_pts[idx]
-
             # Find transformation matrix
             # m = cv2.estimateRigidTransform(prev_pts, curr_pts, fullAffine=False) #will only work with OpenCV-3 or less
             m, _ = cv2.estimateAffinePartial2D(prev_pts, curr_pts)
-
             # Extract traslation
             dx = m[0, 2]
             dy = m[1, 2]
-
             # Extract rotation angle
             da = np.arctan2(m[1, 0], m[0, 0])
-
             # Store transformation
             transforms[i] = [dx, dy, da]
-
             # Move to next frame
             prev_gray = curr_gray
-
         # Compute trajectory using cumulative sum of transformations
         trajectory = np.cumsum(transforms, axis=0)
-
         # Create variable to store smoothed trajectory
         smoothed_trajectory = self.smooth(trajectory, self.smooth_amount)
-
         # Calculate difference in smoothed_trajectory and trajectory
         difference = smoothed_trajectory - trajectory
-
         # Calculate newer transformation array
         transforms_smooth = transforms + difference
-
         # Reset stream to first frame
+        # Starting the stabilization process
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
         # Write n_frames-1 transformed frames
         for i in range(n_frames - 2):
             # Read next frame
             success, frame = cap.read()
             if not success:
                 break
-
             # Extract transformations from the new transformation array
             dx = transforms_smooth[i, 0]
             dy = transforms_smooth[i, 1]
             da = transforms_smooth[i, 2]
-
             # Reconstruct transformation matrix accordingly to new values
             m = np.zeros((2, 3), np.float32)
             m[0, 0] = np.cos(da)
@@ -228,13 +201,10 @@ class VideoSeparation:
             m[1, 1] = np.cos(da)
             m[0, 2] = dx
             m[1, 2] = dy
-
             # Apply affine wrapping to the given frame
             frame_stabilized = cv2.warpAffine(frame, m, (w, h))
-
             # Fix border artifacts
             frame_stabilized = self.fixBorder(frame_stabilized)
-
             cv2.waitKey(10)
             out.write(cv2.rotate(frame_stabilized, cv2.ROTATE_90_CLOCKWISE))
 
